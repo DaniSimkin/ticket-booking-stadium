2f996a3da25a621085cda61edf44383c0eda1ef4
Dani Simkin <danisimkin23@gmail.com>
2022-05-15 18:38:50 +0300

    Init

---
+++ src/main/java/com/dani/ticketServiceStadium/TicketServiceStadiumApplication.java
@@ -0,0 +1,13 @@
+package com.dani.ticketServiceStadium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketServiceStadiumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketServiceStadiumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dani/ticketServiceStadium/TicketServiceStadiumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dani.ticketServiceStadium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketServiceStadiumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
